becce3c0755af1bc920ec76818b1708f83ad0bc7
Brian Ringley <brian.ringley@wework.com>
2018-02-15 21:48:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ def position_taken?(board, index)
   # if board[index].length == 0
     # this will only work if empty board space is "" not " "...'
     
-  if (board[index] == "") || (board[index] == " ")
+  if (board[index] == "") || (board[index] == " ") || nil
     false
   else
     puts "Sorry this position has already been taken, please try another"
