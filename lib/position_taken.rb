9acb4f702bfcb8c3f07b11b34b9ae9f50c8cbfa5
Brian Ringley <brian.ringley@wework.com>
2018-02-15 21:47:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,10 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
+  
   # if board[index].length == 0
     # this will only work if empty board space is "" not " "...'
+    
   if (board[index] == "") || (board[index] == " ")
     false
   else
