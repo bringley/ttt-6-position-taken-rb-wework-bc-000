d665b4f9e886a2b4281ad8dc6b87debcbb5c5688
Brian Ringley <brian.ringley@wework.com>
2018-02-15 22:04:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,6 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  
-  # if board[index].length == 0
-    # this will only work if empty board space is "" not " "...'
-    # also you can't call measure the length of nil because there is no length method for NilClass
-    
   if (board[index] == "") || (board[index] == " ") || (board[index] == nil)
     false
     elsif (board[index] == "X") || (board[index] == "O")
@@ -14,3 +9,7 @@ def position_taken?(board, index)
     puts "Congratulations you found an unanticipated edge case and now this program is broken!"
   end
 end
+
+  # if board[index].length == 0
+    # this will only work if empty board space is "" not " "...'
+    # also you can't call measure the length of nil because there is no length method for NilClass
