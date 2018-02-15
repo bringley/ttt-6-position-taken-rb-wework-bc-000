b66cf543ecb2c13b895e0303ee66bf6bd2573560
Brian Ringley <brian.ringley@wework.com>
2018-02-15 22:00:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,8 +4,9 @@ def position_taken?(board, index)
   
   if board[index].length == 0
     # this will only work if empty board space is "" not " "...'
+    # also you can't call measure the length of nil because there is no length method for NilClass
     
-  # if (board[index] == "") || (board[index] == " ") || (board[index] == nil)
+  if (board[index] == "") || (board[index] == " ") || (board[index] == nil)
     false
   else
     puts "Sorry this position has already been taken, please try another"
