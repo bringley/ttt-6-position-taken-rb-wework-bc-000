66bb57eb1343161dcd9b5d5bc099ade7c1e4b90b
Brian Ringley <brian.ringley@wework.com>
2018-02-15 21:34:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,10 @@
 # code your #position_taken? method here!
 
-def position_taken(index)
-  if board
+def position_taken(index, value = "X")
+  if board[index] == " "
+    board[index] = value
+    # wait it doesn't know wtf board is...
+  else
+    puts "Sorry this position has already been taken, please try another"
+  end
+end
